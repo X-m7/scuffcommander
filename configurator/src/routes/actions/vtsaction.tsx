9a7a50e76bdfb514f5dab6675e3b3d7520b74c19
Prajna Sariputra <putr4.s@gmail.com>
2022-11-26 05:16:06 +1100

    Add support for loading trigger hotkey and load model VTS actions
    Also split out common code for the select type inputs

--- configurator/src/routes/actions/vtsaction.tsx
+++ configurator/src/routes/actions/vtsaction.tsx
@@ -38,6 +38,43 @@ class EditVTSAction extends Component<EditVTSActionProps, EditVTSActionState> {
     this.actionTypeUpdate(this.state.actionType, true);
   }
 
+  fillSelectInput = (
+    actionType: VTSActionType,
+    invokeArgOuter: string,
+    invokeArgInner: string,
+    init: boolean
+  ) => {
+    invoke(invokeArgOuter)
+      .then((listRaw) => {
+        this.setState({
+          ...this.state,
+          actionType,
+          selectInputOptions: listRaw as string[],
+          showSelectInput: true,
+        });
+
+        // On initialisation also convert the loaded ID to the name
+        if (init && this.props.data && this.props.data.content) {
+          invoke(invokeArgInner, {
+            id: this.props.data.content,
+          })
+            .then((actRaw) => {
+              const actionName = actRaw as string;
+              this.setState({
+                ...this.state,
+                selectInputValue: `x-${actionName}`,
+              });
+            })
+            .catch((err) => {
+              this.props.msgFunc(`Error occurred: ${err.toString()}`);
+            });
+        }
+      })
+      .catch((err) => {
+        this.props.msgFunc(`Error occurred: ${err.toString()}`);
+      });
+  };
+
   actionTypeUpdate = (actionType: VTSActionType, init: boolean) => {
     switch (actionType) {
       case VTSActionType.None:
@@ -49,37 +86,28 @@ class EditVTSAction extends Component<EditVTSActionProps, EditVTSActionState> {
         });
         break;
       case VTSActionType.ToggleExpression:
-        // need to convert id to name
-        // also prefill selector list
-        invoke("get_vts_expression_names")
-          .then((listRaw) => {
-            this.setState({
-              ...this.state,
-              actionType,
-              selectInputOptions: listRaw as string[],
-              showSelectInput: true,
-            });
-
-            // On initialisation also convert the loaded ID to the name
-            if (init && this.props.data && this.props.data.content) {
-              invoke("get_vts_expression_name_from_id", {
-                id: this.props.data.content,
-              })
-                .then((actRaw) => {
-                  const actionName = actRaw as string;
-                  this.setState({
-                    ...this.state,
-                    selectInputValue: `x-${actionName}`,
-                  });
-                })
-                .catch((err) => {
-                  this.props.msgFunc(`Error occurred: ${err.toString()}`);
-                });
-            }
-          })
-          .catch((err) => {
-            this.props.msgFunc(`Error occurred: ${err.toString()}`);
-          });
+        this.fillSelectInput(
+          actionType,
+          "get_vts_expression_names",
+          "get_vts_expression_name_from_id",
+          init
+        );
+        break;
+      case VTSActionType.TriggerHotkey:
+        this.fillSelectInput(
+          actionType,
+          "get_vts_hotkey_names",
+          "get_vts_hotkey_name_from_id",
+          init
+        );
+        break;
+      case VTSActionType.LoadModel:
+        this.fillSelectInput(
+          actionType,
+          "get_vts_model_names",
+          "get_vts_model_name_from_id",
+          init
+        );
         break;
       default:
         this.props.msgFunc("Unimplemented VTS action type");
