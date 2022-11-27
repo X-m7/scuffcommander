09e652404b4cf06a9f294bd326c86f38e11eaa9a
Prajna Sariputra <putr4.s@gmail.com>
2022-11-28 07:31:18 +1100

    [WIP] Drag and drop support for chain
    Sometimes issues with styling occur afterwards

--- configurator/src/routes/actions/chainaction.tsx
+++ configurator/src/routes/actions/chainaction.tsx
@@ -2,24 +2,25 @@ import { h, Fragment, Component } from "preact";
 import { useEffect, useState } from "preact/hooks";
 import { invoke } from "@tauri-apps/api";
 
+import style from "./style.css";
 import { Action } from "./types";
 
-interface EditChainActionProps {
-  data?: Action[];
-  msgFunc: (msg: string) => void;
-}
-
-interface EditChainActionState {
-  chain: Action[];
-}
-
 interface ChainElementProps {
+  pos: number;
   data: Action;
   msgFunc: (msg: string) => void;
+  moveCallback: (start: number, end: number) => void;
 }
 
-const ChainElement = ({ data, msgFunc }: ChainElementProps) => {
+const ChainElement = ({
+  pos,
+  data,
+  msgFunc,
+  moveCallback,
+}: ChainElementProps) => {
   const [actionStr, setActionStr] = useState<string | undefined>(undefined);
+  const [dragged, setDragged] = useState<boolean>(false);
+  const [draggedOver, setDraggedOver] = useState<boolean>(false);
 
   useEffect(() => {
     invoke("convert_action_to_string", { action: data })
@@ -31,12 +32,72 @@ const ChainElement = ({ data, msgFunc }: ChainElementProps) => {
       });
   }, [data, msgFunc]);
 
+  const onDragStart = (e: DragEvent) => {
+    if (e.dataTransfer === null) {
+      return;
+    }
+    e.dataTransfer.setData("index", pos.toString());
+    setDragged(true);
+  };
+
+  const onDragEnd = () => {
+    setDragged(false);
+    setDraggedOver(false);
+  };
+
+  const onDrop = (e: DragEvent) => {
+    if (e.dataTransfer === null) {
+      return;
+    }
+    moveCallback(parseInt(e.dataTransfer.getData("index"), 10), pos);
+
+    setDragged(false);
+    setDraggedOver(false);
+  };
+
+  const onDragOver = (e: DragEvent) => {
+    // default is to disable drop so stop that
+    e.preventDefault();
+  };
+
+  const onDragEnter = () => {
+    setDraggedOver(true);
+  };
+
+  const onDragLeave = () => {
+    setDraggedOver(false);
+  };
+
   if (actionStr === undefined) {
     return <Fragment />;
   }
 
-  return <li>{actionStr}</li>;
+  return (
+    <li
+      class={`${dragged ? style.dragged : ""} ${
+        draggedOver ? style.draggedOver : ""
+      }`}
+      draggable
+      onDragStart={onDragStart}
+      onDragEnd={onDragEnd}
+      onDragOver={onDragOver}
+      onDragEnter={onDragEnter}
+      onDragLeave={onDragLeave}
+      onDrop={onDrop}
+    >
+      {actionStr}
+    </li>
+  );
 };
+
+interface EditChainActionProps {
+  data?: Action[];
+  msgFunc: (msg: string) => void;
+}
+
+interface EditChainActionState {
+  chain: Action[];
+}
 
 class EditChainAction extends Component<
   EditChainActionProps,
@@ -45,18 +106,39 @@ class EditChainAction extends Component<
   constructor(props: EditChainActionProps) {
     super(props);
 
+    const chain = this.props.data ?? [];
+
     this.state = {
-      chain: this.props.data ?? [],
+      chain,
     };
   }
+
+  moveItemInChain = (draggedIndex: number, targetIndex: number) => {
+    if (draggedIndex === targetIndex) {
+      return;
+    }
+    const chain = Array.from(this.state.chain);
+
+    // remove the dragged item and add it to the target position
+    const draggedAction = chain.splice(draggedIndex, 1)[0];
+    chain.splice(targetIndex, 0, draggedAction);
+
+    this.setState({ chain });
+  };
 
   render() {
     return (
       <Fragment>
         <ol>
-          {this.state.chain.map((act) => {
+          {this.state.chain.map((act, index) => {
             return (
-              <ChainElement key={act} data={act} msgFunc={this.props.msgFunc} />
+              <ChainElement
+                key={act}
+                pos={index}
+                data={act}
+                msgFunc={this.props.msgFunc}
+                moveCallback={this.moveItemInChain}
+              />
             );
           })}
         </ol>
