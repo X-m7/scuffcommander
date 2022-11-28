04b5a478b1dbce3e6f5f97b074c075580426aba0
Prajna Sariputra <putr4.s@gmail.com>
2022-11-29 04:41:31 +1100

    [WIP] Page details load and button delete
    No rearranging buttons or adding new buttons yet
    
    Also added warning when deleting or renaming home page

--- configurator/src/routes/pages/editor.tsx
+++ configurator/src/routes/pages/editor.tsx
@@ -2,6 +2,8 @@ import { h, Fragment } from "preact";
 import { useEffect, useState } from "preact/hooks";
 import { invoke } from "@tauri-apps/api";
 
+import DraggableListItem from "/components/draggablelistitem";
+
 interface EditPageProps {
   page: string;
   msgFunc: (msg: string) => void;
@@ -14,15 +16,23 @@ const EditPage = ({
   onSaveDeleteCallback,
 }: EditPageProps) => {
   const [pageId, setPageId] = useState<string>("");
+  const [buttonsList, setButtonsList] = useState<string[]>([]);
 
   useEffect(() => {
     if (pageProp === "none" || pageProp === "new") {
       setPageId("");
+      setButtonsList([]);
       return;
     }
 
     const newPageId = pageProp.substring(2);
-    setPageId(newPageId);
+
+    invoke("get_page_buttons_info", { id: newPageId }).then((buttonsRaw) => {
+      const buttons = buttonsRaw as string[];
+
+      setButtonsList(buttons);
+      setPageId(newPageId);
+    });
   }, [pageProp]);
 
   const onPageIdInput = (e: Event) => {
@@ -33,9 +43,16 @@ const EditPage = ({
 
   const deleteCurrentPage = () => {
     const id = pageProp.substring(2);
+
     invoke("delete_page", { id })
       .then(() => {
-        msgFunc(`Page with ID "${id}" deleted`);
+        if (id === "home") {
+          msgFunc(
+            'Warning: The page with ID "home" has been deleted, which is the default page'
+          );
+        } else {
+          msgFunc(`Page with ID "${id}" deleted`);
+        }
         onSaveDeleteCallback();
       })
       .catch((err) => {
@@ -48,8 +65,30 @@ const EditPage = ({
     const currentId = pageProp.substring(2);
     invoke("rename_page", { currentId, newId })
       .then(() => {
-        msgFunc(`Page with ID "${currentId}" renamed to "${newId}"`);
+        if (currentId === "home") {
+          msgFunc(
+            'Warning: The page with ID "home" has been renamed, which is the default page'
+          );
+        } else {
+          msgFunc(`Page with ID "${currentId}" renamed to "${newId}"`);
+        }
         onSaveDeleteCallback();
+      })
+      .catch((err) => {
+        msgFunc(`Error occurred: ${err.toString()}`);
+      });
+  };
+
+  const dataConverter = async (page: string) => {
+    return page;
+  };
+
+  const movePageInList = (draggedIndex: number, targetIndex: number) => {};
+
+  const deleteButtonFromPage = (index: number) => {
+    invoke("delete_button_from_page", { id: pageProp.substring(2), index })
+      .then(() => {
+        setButtonsList(buttonsList.filter((elem, i) => index != i));
       })
       .catch((err) => {
         msgFunc(`Error occurred: ${err.toString()}`);
@@ -80,6 +119,23 @@ const EditPage = ({
           Delete currently selected page
         </button>
       </span>
+      <ol>
+        {buttonsList.map((page, index) => {
+          return (
+            <DraggableListItem<string>
+              key={page}
+              pos={index}
+              data={page}
+              dataConverter={dataConverter}
+              moveCallback={movePageInList}
+            >
+              <button type="button" onClick={() => deleteButtonFromPage(index)}>
+                Delete
+              </button>
+            </DraggableListItem>
+          );
+        })}
+      </ol>
     </Fragment>
   );
 };
